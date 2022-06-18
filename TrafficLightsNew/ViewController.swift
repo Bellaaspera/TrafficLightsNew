fce52d0808edfa8576ca03d4c88cb7f45c58e0ac
Bellaaspera <lanasenatorova@gmail.com>
2022-06-18 18:22:00 +0300

    1. Made storyboard 2. Connected IB to code 3. Rounded angles

--- TrafficLightsNew/ViewController.swift
+++ TrafficLightsNew/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redLightView: UIView!
+    
+    @IBOutlet weak var yellowLightView: UIView!
+    
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var switchingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        switchingButton.layer.cornerRadius = 10
+        redLightView.layer.cornerRadius = 101
+        yellowLightView.layer.cornerRadius = 101
+        greenLightView.layer.cornerRadius = 101
     }
 
-
+    @IBAction func switchingButtonTapped() {
+    }
+    
 }
 
