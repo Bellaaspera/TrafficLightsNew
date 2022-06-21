ee6733883eba23a0ecbabde349b6369ba5b840c6
Bellaaspera <lanasenatorova@gmail.com>
2022-06-21 15:50:34 +0300

    1. SWITCH method working 2. IF method is not working

--- TrafficLightsNew/ViewController.swift
+++ TrafficLightsNew/ViewController.swift
@@ -9,27 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    private enum CurrentLight {
+//        case red, yellow, green
+//    }
     
     @IBOutlet weak var redLightView: UIView!
-    
     @IBOutlet weak var yellowLightView: UIView!
-    
     @IBOutlet weak var greenLightView: UIView!
     
     @IBOutlet weak var switchingButton: UIButton!
     
+    
+//    private var currentLight = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = lightOff
+        yellowLightView.alpha = lightOff
+        greenLightView.alpha = lightOff
         
         switchingButton.layer.cornerRadius = 10
-        redLightView.layer.cornerRadius = 101
-        yellowLightView.layer.cornerRadius = 101
-        greenLightView.layer.cornerRadius = 101
     }
 
-    @IBAction func switchingButtonTapped() {
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
     }
     
+    @IBAction func switchingButtonTapped() {
+        let arrayOfLights: [UIView?] = [redLightView, yellowLightView, greenLightView]
+        if switchingButton.currentTitle == "START" {
+            switchingButton.setTitle("NEXT", for: .normal)
+        }
+         
+//        switch currentLight {
+//        case .red:
+//            greenLightView.alpha = lightOff
+//            redLightView.alpha = lightOn
+//            currentLight = .yellow
+//        case .yellow:
+//            redLightView.alpha = lightOff
+//            yellowLightView.alpha = lightOn
+//            currentLight = .green
+//        case .green:
+//            yellowLightView.alpha = lightOff
+//            greenLightView.alpha = lightOn
+//            currentLight = .red
+//        }
+       
+        for currentLight in arrayOfLights {
+        if  redLightView.alpha == lightOff {
+            currentLight?.alpha = lightOn
+            greenLightView.alpha = lightOff
+           
+        } else if yellowLightView.alpha == lightOff {
+            currentLight?.alpha = lightOn
+            redLightView.alpha = lightOff
+            
+        } else if currentLight?.alpha == lightOff {
+            currentLight?.alpha = lightOn
+            yellowLightView.alpha = lightOff
+            
+        }
+        }
+        
+    }
 }
-
